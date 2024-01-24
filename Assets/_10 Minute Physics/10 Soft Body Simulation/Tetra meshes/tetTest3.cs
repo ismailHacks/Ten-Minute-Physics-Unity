098ed0bf147d3b6a665bafc850ec1dbb5d409daa
ismailHacks <85501604+ismailHacks@users.noreply.github.com>
2024-01-24 18:21:01 +0000

    Update tetTest3.cs

--- Assets/_10 Minute Physics/10 Soft Body Simulation/Tetra meshes/tetTest3.cs
+++ Assets/_10 Minute Physics/10 Soft Body Simulation/Tetra meshes/tetTest3.cs
@@ -16,37 +16,44 @@ public class tetTest3 : TetrahedronData
 	public override int[] GetTetEdgeIds => tetEdgeIds;
 	public override int[] GetTetSurfaceTriIds => tetSurfaceTriIds;
 
-	public float cylinderOuterRadius = 0.5f;
-	public float cylinderInnerRadius = 0.3f;
+	public float cylinderOuterRadius = 1f;
+	public float cylinderInnerRadius = 0.4f;
 
-	private double[] vertsCylinder = new double[12];
+	private double[] vertsCylinder = new double[24];
+	private int[] tetIdsCylinder = new int[8];
+
 
 
 	public tetTest3()
 	{
 		float angleBetweenTets = (2*(cylinderOuterRadius-cylinderInnerRadius)*Mathf.Tan(Mathf.Deg2Rad*30))/cylinderOuterRadius;
+		Debug.Log(angleBetweenTets);
 		angleBetweenTets = 360/(Mathf.Rad2Deg*angleBetweenTets);
 		float angleBetweenTetsMod = 360/(int)angleBetweenTets;
+		Debug.Log(angleBetweenTetsMod+ "   "+ angleBetweenTets);
 		angleBetweenTetsMod = Mathf.Deg2Rad*angleBetweenTetsMod;
-
-		for (int i = 0; i < 1; i++)
+		
+		for (int j = 1; j < 3; j++)
 		{
-			float currentAngle = angleBetweenTetsMod*i;
-			vertsCylinder[0] = cylinderOuterRadius*Mathf.Sin(currentAngle);
-			vertsCylinder[1] = 0;
-			vertsCylinder[2] = cylinderOuterRadius*Mathf.Cos(currentAngle);
+			for (int i = 0; i < 1; i++)
+			{
+				float currentAngle = angleBetweenTetsMod*i;
+				vertsCylinder[0] = cylinderOuterRadius*Mathf.Sin(currentAngle);
+				vertsCylinder[1] = 0;
+				vertsCylinder[2] = cylinderOuterRadius*Mathf.Cos(currentAngle);
 
-			vertsCylinder[3] = cylinderInnerRadius*Mathf.Sin(currentAngle+angleBetweenTetsMod/2);
-			vertsCylinder[4] = 0;
-			vertsCylinder[5] = cylinderInnerRadius*Mathf.Cos(currentAngle+angleBetweenTetsMod/2);
+				vertsCylinder[3] = cylinderInnerRadius*Mathf.Sin(currentAngle+angleBetweenTetsMod/2);
+				vertsCylinder[4] = 0;
+				vertsCylinder[5] = cylinderInnerRadius*Mathf.Cos(currentAngle+angleBetweenTetsMod/2);
 
-			vertsCylinder[6] = cylinderOuterRadius*Mathf.Sin(currentAngle+angleBetweenTetsMod/2);
-			vertsCylinder[7] =0.5f;
-			vertsCylinder[8] = cylinderOuterRadius*Mathf.Cos(currentAngle+angleBetweenTetsMod/2);
+				vertsCylinder[6] = cylinderOuterRadius*Mathf.Sin(currentAngle+angleBetweenTetsMod/2);
+				vertsCylinder[7] =0.5f;
+				vertsCylinder[8] = cylinderOuterRadius*Mathf.Cos(currentAngle+angleBetweenTetsMod/2);
 
-			vertsCylinder[9] = cylinderOuterRadius*Mathf.Sin(currentAngle+angleBetweenTetsMod);
-			vertsCylinder[10] = 0;
-			vertsCylinder[11] = cylinderOuterRadius*Mathf.Cos(currentAngle+angleBetweenTetsMod/2);
+				vertsCylinder[9] = cylinderOuterRadius*Mathf.Sin(currentAngle+angleBetweenTetsMod);
+				vertsCylinder[10] = 0;
+				vertsCylinder[11] = cylinderOuterRadius*Mathf.Cos(currentAngle+angleBetweenTetsMod/2);
+			}
 		}
 
 		verts = new float[vertsCylinder.Length];
@@ -54,18 +61,8 @@ public class tetTest3 : TetrahedronData
 		{
 			verts[i] = (float)vertsCylinder[i];
         }
+
 	}
-	
-	//Vertices (x, y, z) come after each other so divide by 3 to get total vertices
-	//Provides the vertices of each particle in the mesh
-	/*private double[] vertsDouble =
-	{
-		1f,0f,0f, 
-		-0.5f,Mathf.Sqrt(3)/2f,0f, 
-		-0.5f,0f,0f,
-		0f,0f,Mathf.Sqrt(2),
-		0f,0f,-1f
-	};*/
 
 	//Provides the ID position of the vertices that make up a tetrahedron
 	private int[] tetIds =
@@ -77,7 +74,7 @@ public class tetTest3 : TetrahedronData
 	//unlike tetIds the edges should not be repeated with connecting tetrahedrals as they will be looped over.
 	private int[] tetEdgeIds =
 	{
-		0,2, 2,1, 1,0, 1,3, 3,0, 2,3 
+		0,1, 0,2, 0,3, 1,2, 1,3, 2,3
 	};
 
 	//Provides the connections between all surfaces that are visible in order to render a mesh, must be clockwise done when looking at the surface.
